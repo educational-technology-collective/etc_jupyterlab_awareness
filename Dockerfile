79d887c1c343d13a201a8029580a95a5284674f4
null <adpatter@umich.edu>
2022-06-01 13:11:02 -0400

    et alia

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN pip3 install jupyterlab-link-share
 
 ARG DATE
 
-COPY etc_jupyterlab_awareness /tmp/etc_jupyterlab_awareness
+COPY etc_jupyterlab_awareness/dist/etc_jupyterlab_awareness-0.1.0.tar.gz /tmp/etc_jupyterlab_awareness-0.1.0.tar.gz
 
-RUN python3 -m pip install /tmp/etc_jupyterlab_awareness/dist/etc_jupyterlab_awareness-0.1.0.tar.gz
+RUN python3 -m pip install /tmp/etc_jupyterlab_awareness-0.1.0.tar.gz
 
 CMD ["jupyter-labhub"]
--- etc_jupyterlab_awareness/src/index.ts
+++ etc_jupyterlab_awareness/src/index.ts
@@ -11,38 +11,72 @@ import { IStateDB } from '@jupyterlab/statedb';
 
 import { requestAPI } from './handler';
 
+const PLUGIN_ID = '@educational-technology-collective/etc_jupyterlab_awareness:plugin';
 /**
  * Initialization data for the @educational-technology-collective/etc_jupyterlab_awareness extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@educational-technology-collective/etc_jupyterlab_awareness:plugin',
+  id: PLUGIN_ID,
   autoStart: true,
   requires: [ISettingRegistry, INotebookTracker, IStateDB],
-  activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null, notebookTracker: INotebookTracker, stateDB: IStateDB) => {
-    console.log('JupyterLab extension @educational-technology-collective/etc_jupyterlab_awareness is activated!');
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null, notebookTracker: INotebookTracker, stateDB: IStateDB) => {
 
-    let env = await requestAPI<any>('get_example');
-    console.log('env', env);
-    let hub_user = env['os.environ']['JUPYTERHUB_USER'];
-    console.log('hub_user', hub_user);
+    (async () => {
+      console.log('JupyterLab extension @educational-technology-collective/etc_jupyterlab_awareness is activated!');
 
-    await app.restored;
-    
-    let user = stateDB.fetch('hub_user');
+      await app.restored;
 
-    if (!user) {
-      stateDB.save('hub_user', hub_user);
-      document.cookie = `hub_user=${hub_user}; path='/';`;
-    }
+      console.log('document.cookie1', document.cookie)
 
-    notebookTracker.widgetAdded.connect(async (sender: INotebookTracker, notebookPanel: NotebookPanel) => {
+      let env = await requestAPI<any>('get_example');
+      console.log('env', env);
 
-      await notebookPanel.revealed;
-      await notebookPanel.sessionContext.ready;
+      let hubUser = env['os.environ']['JUPYTERHUB_USER'];
+      console.log('hub_user', hubUser);
 
-      let hub_user = document?.cookie?.split('; ')?.find(row => row.startsWith('hub_user='))?.split('=')[1]
+      if (!document?.cookie?.split('; ')?.find(row => row.startsWith('hub_user='))) {
 
-      console.log('hub_user', hub_user);
+        document.cookie = `hub_user=${hubUser}; domain=mentoracademy.org; path=/; max-age=${60*60*24*365};`;
+      }
+
+      console.log('document.cookie2', document.cookie);
+
+      notebookTracker.forEach((notebookPanel: NotebookPanel) => {
+        console.log('forEach');
+
+        console.log('document.cookie', document.cookie);
+
+        let hubUser = document?.cookie?.split('; ')?.find(row => row.startsWith('hub_user='))?.split('=')[1];
+
+        (notebookPanel?.content.model?.sharedModel as any).awareness.setLocalStateField('user', { name: hubUser });
+      });
+
+
+      notebookTracker.widgetAdded.connect(async (sender: INotebookTracker, notebookPanel: NotebookPanel) => {
+        console.log('notebookTracker.widgetAdded');
+
+        await notebookPanel.revealed;
+        await notebookPanel.sessionContext.ready;
+
+        console.log('document.cookie', document.cookie);
+
+        let hubUser = document?.cookie?.split('; ')?.find(row => row.startsWith('hub_user='))?.split('=')[1];
+
+        (notebookPanel?.content.model?.sharedModel as any).awareness.setLocalStateField('user', { name: hubUser });
+
+        console.log('hub_user', hubUser);
+      });
+
+      notebookTracker.currentChanged.connect(async (sender: INotebookTracker, notebookPanel: NotebookPanel | null) => {
+        console.log('notebookTracker.currentChanged')
+
+      })
+
+
+      notebookTracker.widgetUpdated.connect(async (sender: INotebookTracker, notebookPanel: NotebookPanel) => {
+        console.log('notebookTracker.widgetUpdated');
+
+      });
 
       // requestAPI<any>('get_example')
       //   .then(data => {
@@ -58,29 +92,20 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
       // (notebookPanel?.content.model?.sharedModel as any).awareness.setLocalStateField('user', { name: Date.now().toString() });
 
-      (notebookPanel?.content.model?.sharedModel as any).awareness.setLocalStateField('user', { name: hub_user});
 
+      // if (settingRegistry) {
+      //   settingRegistry
+      //     .load(plugin.id)
+      //     .then(settings => {
+      //       settings.user
+      //       console.log('@educational-technology-collective/etc_jupyterlab_awareness settings loaded:', settings.composite);
+      //     })
+      //     .catch(reason => {
+      //       console.error('Failed to load settings for @educational-technology-collective/etc_jupyterlab_awareness.', reason);
+      //     });
+      // }
 
-    });
-
-    notebookTracker.currentChanged.connect(async (sender: INotebookTracker, notebookPanel: NotebookPanel | null) => {
-
-
-    })
-
-    // if (settingRegistry) {
-    //   settingRegistry
-    //     .load(plugin.id)
-    //     .then(settings => {
-    //       console.log('@educational-technology-collective/etc_jupyterlab_awareness settings loaded:', settings.composite);
-    //     })
-    //     .catch(reason => {
-    //       console.error('Failed to load settings for @educational-technology-collective/etc_jupyterlab_awareness.', reason);
-    //     });
-    // }
-
-
-
+    })()
   }
 };
 
